d52a110f3ab18977e84509bacedb7b79f6d8919d
Juanes <juan.rivero@aireeterno.com>
2024-01-14 15:55:11 -0500

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className='Wrapper'>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
